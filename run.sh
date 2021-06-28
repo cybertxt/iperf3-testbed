f201947be89bb68a7c46c3abb218a1a6adab9959
xtao <vertextao@hotmail.com>
2021-06-28 17:52:06 +0800

    remove useless code

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 if [ -z $CCA ]; then
   CCA='reno'
 fi
@@ -8,10 +8,19 @@ fi
 
 TARGET_HOST='10.10.61.58'
 
+echo "Begin pulling data from $TARGET_HOST for ${TEST_DURATION}s by using CCA ${CCA}..."
+
+CCA_SAVE=`ssh root@${TARGET_HOST} "cat /proc/sys/net/ipv4/tcp_congestion_control"`
+## Start iperf server
 ssh root@${TARGET_HOST} "echo $CCA > /proc/sys/net/ipv4/tcp_congestion_control; iperf3 -sD1"
+## Start ping to record the round-trip-latency
 ping -c ${TEST_DURATION} ${TARGET_HOST} > ping.log&
+## Do test
 iperf3 -c ${TARGET_HOST} -R -J -t ${TEST_DURATION} > iperf3.json
-sleep 1
+## Restore the CCA setting
+ssh root@${TARGET_HOST} "echo $CCA_SAVE > /proc/sys/net/ipv4/tcp_congestion_control"
+## Process the result
 grep ttl ping.log | awk '{print $5,$7}' | awk -F'[ =]' '{print $2,$4}' > ping_rtts.log
+## Draw graph
 timestr=`date "+%Y%m%d%H%M"`
-python3 iperf3_plotter.py -e 100 -o "iperf3-${CCA}-${TEST_DURATION}-${timestr}" iperf3.json
+python3 iperf3_plotter.py -e 100 -o "iperf3-${CCA}-${TEST_DURATION}-${timestr}"
